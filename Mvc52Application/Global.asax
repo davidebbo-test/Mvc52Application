b8955a1e1068971317679e69c34a559653a95ee0
davidebbo <david.ebbo@microsoft.com>
2014-11-11 08:39:11 -0800

    Initial commit

---
+++ Mvc52Application/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc52Application.MvcApplication" Language="C#" %>
---
+++ Mvc52Application/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc52Application.Startup))]
+namespace Mvc52Application
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
