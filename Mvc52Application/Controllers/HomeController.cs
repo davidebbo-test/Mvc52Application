accc60d81973f6a307215fdda59bb391927daa31
davidebbo <david.ebbo@microsoft.com>
2014-11-11 08:55:31 -0800

    Add some test tracing to Index action

--- Mvc52Application/Controllers/HomeController.cs
+++ Mvc52Application/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,10 @@ namespace Mvc52Application.Controllers
     {
         public ActionResult Index()
         {
+            Trace.TraceInformation("{0}: This is an informational trace message", DateTime.Now);
+            Trace.TraceWarning("{0}: Here is trace warning", DateTime.Now);
+            Trace.TraceError("{0}: Something is broken; tracing an error!", DateTime.Now);
+
             return View();
         }
 
