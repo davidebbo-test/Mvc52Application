9f98311adaed46346b11aa2d396b8e0087bf1997
David Ebbo <david.ebbo@gmail.com>
2018-04-06 15:17:16 -0700

    Add trace with some quotes

--- Mvc52Application/Controllers/HomeController.cs
+++ Mvc52Application/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace Mvc52Application.Controllers
             Trace.TraceInformation("{0}: This is an informational trace message", DateTime.Now);
             Trace.TraceWarning("{0}: Here is trace warning", DateTime.Now);
             Trace.TraceError("{0}: Something is broken; tracing an error!", DateTime.Now);
+            Trace.TraceInformation("123&quot;456\"789");
 
             return View();
         }
